ec273a39f0d94233f69c329389546115b97df328
mine-martin <wangondumartin2000@gmail.com>
2022-07-20 01:42:20 +0300

    created a task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
+import { title } from 'process';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
+  }
 }
