f58425bdeebc9ae4a0033517e2a72b25ff5c0064
mine <mine@localhost.localdomain>
2022-08-23 12:10:16 +0300

    JWT config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,21 +1,23 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { InjectRepository } from "@nestjs/typeorm";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { JwtPayload } from "./jwtpayload.interface";
-import { User } from "./user.entity";
-import { UsersRepository } from "./users.repository";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from './jwtpayload.interface';
+import { User } from './user.entity';
+import { UsersRepository } from './users.repository';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: "topSecret51",
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    })
+    });
   }
 
   async validate(payload: JwtPayload): Promise<User> {
